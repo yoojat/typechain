4378a37b471cb380701c4e9eb2ec68e434723b44
yoojat <yooja.ty@gmail.com>
2018-07-26 08:58:03 +0900

    Creating a Block part Two

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const CryptoJS = require("crypto-js");
 class Block {
     constructor(index, hash, previousHash, data, timestamp) {
         this.index = index;
@@ -9,9 +10,15 @@ class Block {
         this.timestamp = timestamp;
     }
 }
+// sayHello = () => "Hello"; // Block이 선언되어야지 사용이 가능
+//static으로 선언해야만 클래스 함수로 이용 가능(선언하지 않아도 이용가능)
+Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SH256(index + previousHash + timestamp + data).toString();
 const genesisBlock = new Block(0, "202020202022", "", "Hello", 123456);
-let blockchain = [genesisBlock, genesisBlock];
+let blockchain = [genesisBlock];
 //blockchain은 Block형태를 요소로 하는 배열형태
 // blockchain.push("stuff") //작동되지 않음
+const getBlockchain = () => blockchain;
+const getLatestBlock = () => blockchain[blockchain.length - 1];
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 100);
 console.log(blockchain);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  // sayHello = () => "Hello"; // Block이 선언되어야지 사용이 가능
+
+  //static으로 선언해야만 클래스 함수로 이용 가능(선언하지 않아도 이용가능)
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SH256(index + previousHash + timestamp + data).toString();
 
   constructor(
     index: number,
@@ -22,9 +35,15 @@ class Block {
 
 const genesisBlock: Block = new Block(0, "202020202022", "", "Hello", 123456);
 
-let blockchain: Block[] = [genesisBlock, genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 //blockchain은 Block형태를 요소로 하는 배열형태
 // blockchain.push("stuff") //작동되지 않음
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 100);
 
 console.log(blockchain);
 
