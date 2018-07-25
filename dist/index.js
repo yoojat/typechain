e4e12a96ab810440dc9215048624df712bcea0e5
yoojat <yooja.ty@gmail.com>
2018-07-25 23:57:28 +0900

    Types in Typescript

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
+console.log(sayHi("Nicolas", 44, "male"));
+//# sourceMappingURL=index.js.map
--- index.ts
+++
@@ -1,7 +0,0 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}`;
-};
-
-console.log(sayHi("Nicolas", 44, "male"));
-
-export {};
